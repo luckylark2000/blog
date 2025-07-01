d25bcb8bf48d49e2f6c110707ebdcfbab5be8172
luckylark2000 <3286481191@qq.com>
2025-07-01 22:11:43 +0800

    feat(dailyPractice): 更新0701每日一题，增加一题

--- src/JavaScriptCode/dailyPractice/250701/index.js
+++ src/JavaScriptCode/dailyPractice/250701/index.js
@@ -10,3 +10,30 @@ const result = items
 console.log(result); //BAN-CHE
 
 //知识点：map、filter、reduce
+
+// -------------------------------------
+
+const calculator = {
+  value: 10,
+  add: function (x) {
+    return this.value + x;
+  },
+  multiply: function (x) {
+    return this.value * x;
+  },
+};
+
+const add5 = calculator.add;
+const double = calculator.multiply.bind(calculator);
+const triple = calculator.multiply.bind({ value: 3 });
+console.log(add5(2));
+console.log(double(3));
+console.log(triple(4));
+
+console.log(add5(2) + double(3) + triple(4));
+
+// add5(2) = undefined + 2 = NaN
+// double(3) = 10 * 3 = 30
+// triple(4) = 3 * 4 = 12
+
+//考察知识点：this、bind、undefined + 数字、NaN
